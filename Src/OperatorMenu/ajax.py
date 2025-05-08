84051085bf33c18b44beb267e5615ce1534b4342
kattyshih <146812622+katthshih@users.noreply.github.com>
2025-05-08 18:03:05 +0800

    update

--- Src/OperatorMenu/ajax.py
+++ Src/OperatorMenu/ajax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:UTF-8 -*-
 
+import os
 from flask import (Flask, 
                     render_template,
                     redirect, 
@@ -11,45 +12,84 @@ from flask import (Flask,
 import configparser
 
 app = Flask(__name__)
-# config = configparser.ConfigParser()
-
+configfile = 'setting.ini'
+config = configparser.ConfigParser()
 
 @app.route("/data")
 def homebase():
+    global configfile
+
+    # if configure file for development, ..
+    if os.path.isfile('./static/data/setting.ini'):
+        configfile = './static/data/setting.ini'
+    # if configure file for product, ..
+    elif os.path.isfile('../../data/setting.ini'):
+        configfile = './static/data/setting.ini'
+    else:
+        print('configure file not found!!')
+        return f'configure file not found!'
+
+    try:
+        # for development
+        config.read(configfile, encoding='UTF-8')
+    except Exception as e:    
+        print('except:', e)
+        return f'configure file ead rerror! {e}'
+
     return render_template("data.html")
 
 @app.route("/data/message", methods=["GET"])
 def getDataSetting():
-    config = configparser.ConfigParser()                   
+    #config = configparser.ConfigParser()                   
     if request.method == "GET": 
         data = {}
-        config.read('static/data/setting.ini', encoding='UTF-8')
-        
+        '''
+        # if configure file for development, ..
+        if os.path.isfile('./static/data/setting.ini'):
+            configfile = './static/data/setting.ini'
+        # if configure file for product, ..
+        elif os.path.isfile('../../data/setting.ini'):
+            configfile = './static/data/setting.ini'
+        else:
+            print('configure file not found!!')
+            return f'configure file not found!'
 
-        data = { 
-            "TITLE": config['BASIC']['TITLE'],
-            "REMARK": config['BASIC']['REMARK'],
-            "TIME_ZONE_LIST": config['BASIC']['TIME_ZONE_LIST'],
-            "TIME_ZONE": config['BASIC']['TIME_ZONE'],
-            "TIME_FORMAT_LIST": config['BASIC']['TIME_FORMAT_LIST'],
-            "TIME_FORMAT": config['BASIC']['TIME_FORMAT'],
-            "CURRENCY_SYMBOL_LIST": config['BASIC']['CURRENCY_SYMBOL_LIST'],
-            "CURRENCY_SYMBOL": config['BASIC']['CURRENCY_SYMBOL'],
-            "CURRENCY_SYMBOL_PRINT_SIDE": config['BASIC']['CURRENCY_SYMBOL_PRINT_SIDE'],
-            "THOUSAND_SEPARATOR": config['BASIC']['THOUSAND_SEPARATOR'],
-            "HOME_URL": config['BASIC']['HOME_URL'],
-            "HOME_URL1": config['BASIC']['HOME_URL1'],
-            "HOME_URL2": config['BASIC']['HOME_URL2'],
-            "ASSET": config['BASIC']['ASSET'],
-            "BOOTPROT": config['NETWORK']['BOOTPROT'],
-            "IPADDR": config['NETWORK']['IPADDR'],
-            "NETMASK": config['NETWORK']['NETMASK'],
-            "GATEWAY": config['NETWORK']['GATEWAY'],
-            "ACCOUNT": config['REGISTER']['ACCOUNT'],
-            "TOKEN": config['REGISTER']['TOKEN'],
-            "TOKEN_LAST_TS": config['REGISTER']['TOKEN_LAST_TS'],
-            # "BILL_ACCEPTOR_PORT": config['DEVICE']['BILL_ACCEPTOR_PORT']            
-        }
+        try:
+            # for development
+            config.read(configfile, encoding='UTF-8')
+        except Exception as e:    
+            print('except:', e)
+            return f'configure file ead rerror! {e}'
+        '''
+
+        try:
+            data = { 
+                "TITLE": config['BASIC']['TITLE'],
+                "REMARK": config['BASIC']['REMARK'],
+                "TIME_ZONE_LIST": config['BASIC']['TIME_ZONE_LIST'],
+                "TIME_ZONE": config['BASIC']['TIME_ZONE'],
+                "TIME_FORMAT_LIST": config['BASIC']['TIME_FORMAT_LIST'],
+                "TIME_FORMAT": config['BASIC']['TIME_FORMAT'],
+                "CURRENCY_SYMBOL_LIST": config['BASIC']['CURRENCY_SYMBOL_LIST'],
+                "CURRENCY_SYMBOL": config['BASIC']['CURRENCY_SYMBOL'],
+                "CURRENCY_SYMBOL_PRINT_SIDE": config['BASIC']['CURRENCY_SYMBOL_PRINT_SIDE'],
+                "THOUSAND_SEPARATOR": config['BASIC']['THOUSAND_SEPARATOR'],
+                "HOME_URL": config['BASIC']['HOME_URL'],
+                "HOME_URL1": config['BASIC']['HOME_URL1'],
+                "HOME_URL2": config['BASIC']['HOME_URL2'],
+                "ASSET": config['BASIC']['ASSET'],
+                "BOOTPROT": config['NETWORK']['BOOTPROT'],
+                "IPADDR": config['NETWORK']['IPADDR'],
+                "NETMASK": config['NETWORK']['NETMASK'],
+                "GATEWAY": config['NETWORK']['GATEWAY'],
+                "ACCOUNT": config['REGISTER']['ACCOUNT'],
+                "TOKEN": config['REGISTER']['TOKEN'],
+                "TOKEN_LAST_TS": config['REGISTER']['TOKEN_LAST_TS'],
+                # "BILL_ACCEPTOR_PORT": config['DEVICE']['BILL_ACCEPTOR_PORT']            
+            }
+        except Exception as e:
+            print('except:', e)
+            return f'configure file key error! {e}'
 
         data = json.dumps(data)
         return jsonify(data)  # 直接回傳 data 也可以，都是 json 格式
@@ -57,9 +97,10 @@ def getDataSetting():
 
 @app.route('/data/<section_id>', methods=['POST'])
 def setIniData(section_id):
-
+    '''
     config = configparser.ConfigParser()
     config.read('static/data/setting.ini', encoding='UTF-8') 
+    '''
 
     # NETWORK
     if section_id == "NETWORK":
@@ -75,6 +116,7 @@ def setIniData(section_id):
         # update
         config.set(section_id, 'BOOTPROT', bootprot)
         if bootprot == "static":
+            # for reference: config[section_id]['IPADDR'] = ipaddr
             config.set(section_id, 'IPADDR', ipaddr)
             config.set(section_id, 'NETMASK', netmask)
             config.set(section_id, 'GATEWAY', gateway)
@@ -112,8 +154,13 @@ def setIniData(section_id):
     else:
         pass
 
-    with open('static/data/setting.ini', 'w') as configfile:
-        config.write(configfile)
+    try:
+        with open(configfile, 'w') as f:
+            config.write(f, space_around_delimiters=False)
+    except Exception as e:
+        print('except:', e)
+        # return and show message
+        return f"Drop {e}"
     
     # 'homebase' is the function name for your /data route
     if section_id == "REGISTER" and request.form['submit_button'] == "drop":
